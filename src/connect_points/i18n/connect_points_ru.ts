4730488549c4bc7e74f6085387c8bdeaed1b7ffa
Valeriy Remeshevskiy <remeshevskiy@gmail.com>
2024-11-02 11:28:27 +0300

    Update translation

--- src/connect_points/i18n/connect_points_ru.ts
+++ src/connect_points/i18n/connect_points_ru.ts
@@ -2,103 +2,164 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="ru_RU">
 <context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../about_dialog.py" line="52"/>
+        <source> by NextGIS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="170"/>
+        <source>REPORT_END</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="175"/>
+        <source>Developers</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="176"/>
+        <source>Homepage</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="177"/>
+        <source>Please report bugs at</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="179"/>
+        <source>bugtracker</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="180"/>
+        <source>Video with an overview of the plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="181"/>
+        <source>Other helpful services by NextGIS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="182"/>
+        <source>Convenient up-to-date data extracts for any place in the world</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="185"/>
+        <source>Fully featured Web GIS service</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>AboutDialogBase</name>
     <message>
-        <location filename="../about_dialog_base.ui" line="14"/>
+        <location filename="../ui/about_dialog_base.ui" line="14"/>
         <source>About {plugin_name}</source>
         <translation>О {plugin_name}</translation>
     </message>
     <message>
-        <location filename="../about_dialog_base.ui" line="27"/>
-        <source>&lt;p align=&quot;center&quot;&gt;{plugin_name}&lt;/p&gt;</source>
-        <translation>&lt;p align=&quot;center&quot;&gt;{plugin_name}&lt;/p&gt;</translation>
+        <location filename="../ui/about_dialog_base.ui" line="33"/>
+        <source>Information</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../about_dialog_base.ui" line="40"/>
-        <source>&lt;p&gt;{description}&lt;/p&gt;
-&lt;p&gt;{about}&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Developers:&lt;/b&gt; &lt;a href=&quot;{main_url}/{utm}&quot;&gt;{authors}&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Homepage:&lt;/b&gt; &lt;a href=&quot;{homepage_url}&quot;&gt;{homepage_url}&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Video with an overview of the plugin:&lt;/b&gt; &lt;a href=&quot;{video_url}&quot;&gt;
-{video_url}&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Please report bugs at&lt;/b&gt; &lt;a href=&quot;{tracker_url}&quot;&gt;bugtracker&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;Other helpful services by NextGIS:
-&lt;ul&gt;&lt;li&gt;&lt;b&gt;Convenient up-to-date data extracts for any place in the world:
-&lt;a href=&quot;{main_url}/{utm}&quot;&gt;{main_url}&lt;/a&gt;&lt;/b&gt;&lt;/li&gt;
-&lt;li&gt;&lt;b&gt;Fully featured Web GIS service:
-&lt;a href=&quot;{main_url}/pricing-base{utm}&quot;&gt;
-{main_url}/pricing-base&lt;/a&gt;&lt;/b&gt;&lt;/li&gt;&lt;/ul&gt;
-&lt;/p&gt;</source>
-        <translation>&lt;p&gt;{description}&lt;/p&gt;
-&lt;p&gt;{about}&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Разработчики:&lt;/b&gt; &lt;a href=&quot;{main_url}/{utm}&quot;&gt;{authors}&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Домашняя страница:&lt;/b&gt; &lt;a href=&quot;{homepage_url}&quot;&gt;{homepage_url}&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Видео с обзором плагина:&lt;/b&gt; &lt;a href=&quot;{video_url}&quot;&gt;
-{video_url}&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Пожалуйста, сообщайте о багах в&lt;/b&gt; &lt;a href=&quot;{tracker_url}&quot;&gt;багтрекер&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;Другие полезные сервисы NextGIS:
-&lt;ul&gt;&lt;li&gt;&lt;b&gt;Удобная выборка актуальных данных из любой точки мира:
-&lt;a href=&quot;{main_url}/{utm}&quot;&gt;{main_url}&lt;/a&gt;&lt;/b&gt;&lt;/li&gt;
-&lt;li&gt;&lt;b&gt;Полнофункциональный веб-ГИС-сервис:
-&lt;a href=&quot;{main_url}/pricing-base{utm}&quot;&gt;
-{main_url}/pricing-base&lt;/a&gt;&lt;/b&gt;&lt;/li&gt;&lt;/ul&gt;
-&lt;/p&gt;</translation>
+        <location filename="../ui/about_dialog_base.ui" line="54"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/about_dialog_base.ui" line="72"/>
+        <source>License</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/about_dialog_base.ui" line="94"/>
+        <source>Components</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/about_dialog_base.ui" line="116"/>
+        <source>Contributors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/about_dialog_base.ui" line="158"/>
+        <source>{plugin_name}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/about_dialog_base.ui" line="168"/>
+        <source>Version {version}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/about_dialog_base.ui" line="203"/>
+        <source>Support us</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>ConnectPoints</name>
     <message>
-        <location filename="../qgis_connect_points.py" line="82"/>
+        <location filename="../qgis_connect_points.py" line="83"/>
         <source>Settings</source>
         <translation>Настройки</translation>
     </message>
     <message>
-        <location filename="../qgis_connect_points.py" line="198"/>
+        <location filename="../qgis_connect_points.py" line="242"/>
         <source>Layer with name &apos;%s&apos; not found!</source>
         <translation>Слой с именем '%s' не найден!</translation>
     </message>
     <message>
-        <location filename="../qgis_connect_points.py" line="161"/>
+        <location filename="../qgis_connect_points.py" line="202"/>
         <source>Plugin settings are incorrect. Please, check settings!</source>
-        <translation>Плагин настроен не корректно. Проверте настройки!</translation>
+        <translation>Плагин настроен не корректно. Проверьте настройки!</translation>
     </message>
     <message>
-        <location filename="../qgis_connect_points.py" line="224"/>
+        <location filename="../qgis_connect_points.py" line="272"/>
         <source>Layer with name &apos;%s&apos; can not be used for result output!</source>
         <translation>Слой с именем '%s' не может быть использован для вывода результата!</translation>
     </message>
     <message>
-        <location filename="../qgis_connect_points.py" line="233"/>
+        <location filename="../qgis_connect_points.py" line="284"/>
         <source>Prepare</source>
         <translation>Подготовка</translation>
     </message>
     <message>
-        <location filename="../qgis_connect_points.py" line="234"/>
+        <location filename="../qgis_connect_points.py" line="285"/>
         <source>Calculation</source>
         <translation>Идёт расчет</translation>
     </message>
     <message>
-        <location filename="../qgis_connect_points.py" line="248"/>
+        <location filename="../qgis_connect_points.py" line="306"/>
         <source>Process %d points from %d</source>
         <translation>Обработано %d точек из %d</translation>
     </message>
     <message>
-        <location filename="../qgis_connect_points.py" line="198"/>
+        <location filename="../qgis_connect_points.py" line="242"/>
         <source>New layer is created!</source>
         <translation>Создан новый слой!</translation>
     </message>
     <message>
-        <location filename="../qgis_connect_points.py" line="74"/>
+        <location filename="../qgis_connect_points.py" line="75"/>
         <source>Connect points</source>
         <translation>Соединить точки</translation>
     </message>
     <message>
-        <location filename="../qgis_connect_points.py" line="140"/>
+        <location filename="../qgis_connect_points.py" line="158"/>
         <source>There are no available vector layers in layer tree!</source>
         <translation>В дереве слоев нет доступных векторных слоев!</translation>
     </message>
     <message>
-        <location filename="../qgis_connect_points.py" line="90"/>
+        <location filename="../qgis_connect_points.py" line="91"/>
         <source>About plugin…</source>
         <translation>О плагине…</translation>
     </message>
@@ -106,7 +167,7 @@
 <context>
     <name>Dialog</name>
     <message>
-        <location filename="../dialog.py" line="68"/>
+        <location filename="../dialog.py" line="70"/>
         <source>Point layer &apos;FROM&apos;</source>
         <translation>Точечный слой 'ОТ'</translation>
     </message>
@@ -116,27 +177,27 @@
         <translation>Точечный слой 'К'</translation>
     </message>
     <message>
-        <location filename="../dialog.py" line="133"/>
+        <location filename="../dialog.py" line="136"/>
         <source>Save result in layer</source>
         <translation>Результат сохранить в слой</translation>
     </message>
     <message>
-        <location filename="../dialog.py" line="238"/>
+        <location filename="../dialog.py" line="247"/>
         <source>Layer with name &apos;%s&apos; not found!</source>
         <translation>Слой с именем '%s' не найден!</translation>
     </message>
     <message>
-        <location filename="../dialog.py" line="262"/>
+        <location filename="../dialog.py" line="271"/>
         <source>Choose file for save result</source>
         <translation>Выбирите файл для сохранения результата</translation>
     </message>
     <message>
-        <location filename="../dialog.py" line="102"/>
+        <location filename="../dialog.py" line="100"/>
         <source>Point &apos;FROM&apos; id field</source>
         <translation>Идентификатор точки 'ОТ'</translation>
     </message>
     <message>
-        <location filename="../dialog.py" line="113"/>
+        <location filename="../dialog.py" line="112"/>
         <source>Link field</source>
         <translation>Поле для связи</translation>
     </message>
